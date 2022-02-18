7b3c7180b6476ad2996320e5526990c876d6ed31
Adriana Broadnax <alwaysadrianna@gmail.com>
2022-02-18 17:42:33 -0500

    navbar

---
+++ port2.0/src/components/Navbar/MenuItem.js
@@ -0,0 +1,22 @@
+export const menuItems = [
+    {
+        title: 'About Me',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Portfolio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Resume',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
---
+++ port2.0/src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { menuItems } from "./MenuItem";
+import './Navbar.css'
+
+class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked})
+    }
+
+    render(){
+        return(
+            <nav className="navBarItems">
+                <h1 className="navbar-logo">Portfolio</h1> 
+                {/* <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas-fa-times' : 'fas fa-bars'}></i>
+                    
+                </div> */}
+                <ul>
+                    {menuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}> 
+                                {item.title}
+                                </a>
+                        </li> 
+                        )
+                    })
+                    }
+                </ul>
+            
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
